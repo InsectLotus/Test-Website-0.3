9a911dc04121256928a9ce78f9df01e09df0ad30
InsectLotus <hosingersimon0@gmail.com>
2025-09-22 00:36:01 +0200

    Hauptsächlich verbesserungen und fehler ausbessern und einen Password generator eingebaut

--- scrollAnimation.js
+++
@@ -1,9 +0,0 @@
-
-window.addEventListener('scroll', function () {
-    const elements = document.querySelectorAll('.fade-in');
-    elements.forEach(function (element) {
-        if (element.getBoundingClientRect().top < window.innerHeight) {
-            element.classList.add('visible');
-        }
-    });
-});
